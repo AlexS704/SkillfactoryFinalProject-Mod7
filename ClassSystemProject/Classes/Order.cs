4378e34684ba6752460ea976a7408975ae83b6cc
AlexandrPogorelov <alexandr.pogorelov91@gmail.com>
2024-04-03 23:22:48 +0700

    Созданы ранее реализованные классы

---
+++ ClassSystemProject/Classes/Order.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassSystemProject.Classes
+{
+    class Order <TDelivery, TStruct> where TDelivery : Delivery
+    {
+        public TDelivery Delivery;
+
+        public int Number;
+
+        public string Description;
+
+        public void DisplayAddress()
+        {
+            Console.WriteLine(Delivery.Address);
+        }
+    }
+}
