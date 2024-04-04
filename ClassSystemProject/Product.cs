1c8153b49eba6512976211b9cf5000acdd12d9f1
AlexandrPogorelov <alexandr.pogorelov91@gmail.com>
2024-04-05 00:24:22 +0700

    Добавлены новые классы

---
+++ ClassSystemProject/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassSystemProject
+{
+    abstract class Product
+    {
+        public string Name;
+        public int SelectNumber;
+
+    }
+}
