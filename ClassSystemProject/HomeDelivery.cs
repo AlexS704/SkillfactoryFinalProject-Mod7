95c6c5e31bb192451e20326e78c5e7ff5d8b3836
Alexandr Pogorelov <alexandr.pogorelov91@gmail.com>
2024-07-27 19:39:10 +0700

    Работа с классом HomeDelivery

--- ClassSystemProject/HomeDelivery.cs
+++ ClassSystemProject/HomeDelivery.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +12,7 @@ namespace ClassSystemProject
     {
         string Name = "Иван";
         string Surname = "Петров";
-        int Age = 18; //добавить проверку на 0
+        private int age = 18; //добавить проверку на 0
 
        
           public HomeDelivery(string country, string city, string street, string numberHouse, DateTime DateDelivery) : base(DateDelivery)
@@ -23,15 +24,35 @@ namespace ClassSystemProject
                         
         }
 
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+
+            set
+            {
+                if (value < 18)
+
+                {
+                    Console.WriteLine("Для оформления доставки вам должно быть не меньше 18 лет");
+                }
+
+                else
+                {
+                    age = value;
+                }
+            }
+
+        }
+
         public HomeDelivery(string name, string surname, int age) 
         {
             name = Name;
             surname = Surname;
             
 
-            if (age < 18) { Console.WriteLine("Вам нет 18 лет. Заказ невозможен"); }
-
-            else age = Age;
 
         }
      
